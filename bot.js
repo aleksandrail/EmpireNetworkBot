b9cbfcd9e568643586c27240804d236990729021
aleksandrail <54113006+aleksandrail@users.noreply.github.com>
2019-08-17 02:29:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const bot = new Discord.Client({disableEveryone: true});
+
 client.on('ready', () => {
     console.log('I am ready!');
   bot.user.setActivity("tutorials on TSC", {type: "WATCHING"});  
